9342924415c8c9404b4e65d8558647682174f4c9
Liubomyr Tymchuk <llyb4ikk@gmail.com>
2021-04-14 19:49:43 +0300

    add basic

---
+++ FreightTransport_DAL/Entities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using FreightTransport_DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreightTransport_DAL.Entities
+{
+    public class BaseEntity : IEntity<int>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ FreightTransport_DAL/Entities/Cargo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreightTransport_DAL.Entities
+{
+    public class Cargo : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Weight { get; set; }
+        public float? Height { get; set; }
+        public float? Width { get; set; }
+        public float? Length { get; set; }
+
+        public int ClientId { get; set; }
+        public Client Client { get; set; }
+
+    }
+}
